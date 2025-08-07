e9c491ac23792b97f6213eed3dd4df1f07ed398a
Alexander Grachev <sashagrachev2005@gmail.com>
2025-08-07 16:56:04 +0300

    Commit on 2025-08-07_16:56:04

---
+++ fast_install/complex_install/teleoperation_case.bash
@@ -0,0 +1,6 @@
+cd ../part_install
+chmod +x install_unitree_h1_control_ws.bash
+chmod +x install_unitree_h1_teleopretion_ws.bash
+
+bash install_unitree_h1_control_ws.bash
+bash install_unitree_h1_teleopretion_ws.bash
---
+++ fast_install/part_install/install_unitree_h1_control_ws.bash
@@ -0,0 +1,18 @@
+start_path=$(pwd)
+cd ../..
+mkdir -p unitree_h1_control_ws/src
+cd unitree_h1_control_ws/src
+git clone https://github.com/cyberbanana777/unitree_h1_control_ws.git .
+
+chmod +x install_dependensies.bash
+./install_dependensies.bash
+
+cd ..
+colcon build
+source install/setup.bash
+
+line_to_add="source \"$(pwd)/install/setup.bash\""
+grep -qxF "$line_to_add" ~/.bashrc || echo "$line_to_add" >> ~/.bashrc
+
+cd $start_path
+unset start_path
