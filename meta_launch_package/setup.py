3a224da82ac99358465b9bc799ba4279b85c9908
Alexander Grachev <sashagrachev2005@gmail.com>
2025-08-14 17:57:00 +0300

    Add LICENSE in repo and package.

--- meta_launch_package/setup.py
+++ meta_launch_package/setup.py
@@ -19,7 +19,7 @@ setup(
     maintainer='banana-killer',
     maintainer_email='sashagrachev2005@gmail.com',
     description='A package for meta-launch files, which are used to launch other packages',
-    license='TODO: License declaration',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
